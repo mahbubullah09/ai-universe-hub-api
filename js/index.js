51aa1a8b60986c9d9f11a49f1de562fc7535c798
Mahbubullah <mahbubullahpathan@gmail.com>
2023-08-31 19:13:40 +0600

    modal left js 1.00

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const displayCard = (ai) =>{
         const aiCard = document.createElement('div');
         aiCard.innerHTML= `
 
-        <div onclick="handleModal('${card.id} ')" 
+        <div onclick="handleModal('${card.id}')" 
         class="p-4 border border-gray-400 border-solid rounded-md cursor-pointer">
         <figure class="rounded-md">
             <img src=${card.image} alt="">
@@ -54,18 +54,97 @@ const displayCard = (ai) =>{
         
     });
 
-    const handleModal= (id) =>{
-
-
-
-        my_modal_3.showModal();
-
-    }
 
 
 
 
 
+}
+
+
+
+const handleModal= async(ID) =>{
+    console.log(ID);
+
+    const data= await fetch(`https://openapi.programming-hero.com/api/ai/tool/${ID}`);
+    const modal= await data.json();
+    // console.log(modal);
+    const modalData= modal.data;
+    // console.log(modalData.tool_name);
+    const modalId= document.getElementById('modal-d');
+
+    modalId.innerHTML='';
+
+    
+    const modalDetails= document.createElement('div');
+   
+    modalDetails.innerHTML=`
+
+
+    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 px-2">
+        <div class="left bg-[#0d6efd] bg-opacity-5 border border-solid border-[#EB5757] border-opacity-50 rounded-md p-6">
+
+            <h4 class="text-xl font-semibold text-black  ">${modalData.description}</h4>
+            <div class="grid  grid-cols-1  md:grid-cols-3 gap-2 text-center my-4 items-center ">
+                <div class="bg-white p-3 font-bold text-sm rounded-md text-[#03A30A] h-24 ">
+                    <h4 class="item-center py-5">${modalData.pricing[0].price} ${modalData.pricing[0].plan}</h4>
+                </div>
+                <div class="bg-white p-3 font-bold text-sm rounded-md text-[#F28927]  h-24 ">
+                    <h4 class="item-center py-5">${modalData.pricing[1].price} ${modalData.pricing[1].plan}</h4>
+                </div>
+                <div class="bg-white p-3 font-bold text-sm rounded-md text-[#EB5757] h-24 ">
+                    <h4>${modalData.pricing[2].price} ${modalData.pricing[2].plan}</h4>
+                </div>
+            </div>
+
+            <div class="flex flex-col md:flex-row justify-between ">
+                <div class="left">
+                    <h4 class="font-bold text-xl mb-2">Features</h4>
+
+                    <ul class="list-disc text-xs text-[#585858]  pl-4">
+                        <li>${modalData.features.feature_name}</li>
+                        <li>Multilingual support </li>
+                        <li>Seamless integration</li>
+                    </ul>
+                </div>
+                <div class="right">
+                    <h4 class="font-bold text-xl mb-2">Integrations</h4>
+
+                    <ul class="list-disc text-xs text-[#585858]  pl-4">
+                        <li>FB Messenger</li>
+                        <li>Slack </li>
+                        <li>Telegram</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="right border border-solid border-gray-300 rounded-md p-3 text-center">
+            <figure class="relative">
+                <img src="./Rectangle 23.png" alt="">
+                <div class="bg-[#EB5757] text-white text-xs rounded-md absolute top-1 right-1 px-4 py-1">
+                    <p>94% accuracy</p>
+                </div>
+            </figure>
+            <h4 class="text-xl font-semibold text-black my-3">Hi, how are you doing today?</h4>
+            <p class="text-[#585858] text-sm px-4">I'm doing well, thank you for asking. How can I assist you today?</p>
+
+
+        </div>
+
+
+
+    </div>
+    
+    
+    
+    `
+
+    modalId.appendChild(modalDetails);
+
+
+    my_modal_3.showModal();
 }
 
 loadData();
